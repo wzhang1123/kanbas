b7b7ca8ef3b2205c4d684cc74971cd40cf1fe211
William Zhang <wzhang12321@gmail.com>
2024-02-21 18:20:10 -0500

    finished 2.2.1

---
+++ src/Labs/a3/index.tsx
@@ -0,0 +1,11 @@
+function Assignment3() {
+    return (
+        <div className="Container">
+            <h1>
+                Assignment 3
+            </h1>
+        </div>
+    );
+}
+
+export default Assignment3;
---
+++ src/Labs/index.tsx
@@ -0,0 +1,12 @@
+import Assignment3 from "./a3"
+import JavaScript from "./a3/JavaScript";
+
+function Labs() {
+    return (
+       <div>
+          <Assignment3/>
+          <JavaScript/>
+       </div>
+    );
+ }
+ export default Labs;
