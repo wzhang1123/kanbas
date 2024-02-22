4e86e94e1b69bd41d068c5d27729556f74036cc6
William Zhang <wzhang12321@gmail.com>
2024-02-22 14:30:21 -0500

    finished 2.6

---
+++ src/Labs/a3/ConditionOutput/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInline from "./ConditionalOutputInLine";
+const ConditionalOutput = () => {
+  return (
+    <>
+      <ConditionalOutputIfElse />
+      <ConditionalOutputInline />
+    </>
+  );
+};
+export default ConditionalOutput;
--- src/Labs/a3/index.tsx
+++ src/Labs/a3/index.tsx
@@ -1,10 +1,15 @@
 import JavaScript from "./JavaScript";
 import PathParameters from "./routing/PathParameters";
 import Classes from "./Classes";
+import Styles from "./Styles";
+import ConditionalOutput from "./ConditionOutput";
+
 function Assignment3() {
   return (
     <div>
       <h1>Assignment 3</h1>
+      <ConditionalOutput />
+      <Styles />
       <Classes />
       <PathParameters />
       <JavaScript />
