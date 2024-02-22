e3b02a0c980c2193c2803e31f2780a1f122c7503
William Zhang <wzhang12321@gmail.com>
2024-02-22 14:26:02 -0500

    finished 2.4

--- src/Labs/a3/index.tsx
+++ src/Labs/a3/index.tsx
@@ -1,11 +1,15 @@
+import JavaScript from "./JavaScript";
+import PathParameters from "./routing/PathParameters";
+import Classes from "./Classes";
 function Assignment3() {
-    return (
-        <div className="Container">
-            <h1>
-                Assignment 3
-            </h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Assignment 3</h1>
+      <Classes />
+      <PathParameters />
+      <JavaScript />
+    </div>
+  );
 }
 
 export default Assignment3;
