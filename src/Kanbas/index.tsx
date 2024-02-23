7578a06cf8380b0dbf537100d97e9109b7cd15f5
William Zhang <wzhang12321@gmail.com>
2024-02-23 18:13:11 -0500

    small adjustment to navbar

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -4,12 +4,15 @@ import KanbasNavigation from "./Navigation";
 import Dashboard from "./Dashboard";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Courses from "./Courses";
+import "./index.css";
 
 function Kanbas() {
   return (
-    <div className="d-flex">
-      <KanbasNavigation />
-      <div style={{ flexGrow: 1 }}>
+    <div className="d-flex nav-bar">
+      <div className="kanbas-nav">
+        <KanbasNavigation />
+      </div>
+      <div style={{ flexGrow: 1 }} className="kanbas-content">
         <Routes>
           <Route path="/" element={<Navigate to="Dashboard" />} />
           <Route path="Account" element={<h1>Account</h1>} />
