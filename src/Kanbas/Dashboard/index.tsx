440a813543741894f4b984307a4c90f1639d0c88
William Zhang <wzhang12321@gmail.com>
2024-03-19 19:49:48 -0400

    finished 3.2

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,10 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { courses } from "../Database";
-function Dashboard() {
+
+function Dashboard({
+  courses,
+  course,
+  setCourse,
+  addNewCourse,
+  deleteCourse,
+  updateCourse,
+}: {
+  courses: any[];
+  course: any;
+  setCourse: (course: any) => void;
+  addNewCourse: () => void;
+  deleteCourse: (course: any) => void;
+  updateCourse: () => void;
+}) {
+  // const [_courses, setCourses] = useState(courses);
+  // const [course, setCourse] = useState({
+  //   _id: "0",
+  //   name: "New Course",
+  //   number: "New Number",
+  //   startDate: "2023-09-10",
+  //   endDate: "2023-12-15",
+  //   image: "/images/reactjs.jpg",
+  // });
+
+  // const updateCourse = () => {
+  //   setCourses(
+  //     courses.map((c) => {
+  //       if (c._id === course._id) {
+  //         return course;
+  //       } else {
+  //         return c;
+  //       }
+  //     })
+  //   );
+  // };
+
+  // const addNewCourse = () => {
+  //   const newCourse = { ...course, _id: new Date().getTime().toString() };
+  //   setCourses([...courses, { ...course, ...newCourse }]);
+  // };
+
+  // const deleteCourse = (courseId: string) => {
+  //   setCourses(courses.filter((course) => course._id !== courseId));
+  // };
+
   return (
     <div className="p-4">
-      <h1>Dashboard</h1> <hr />
+      <h1>Dashboard</h1>
+      <h5>Course</h5>
+      <input
+        value={course.name}
+        className="form-control"
+        onChange={(e) => setCourse({ ...course, name: e.target.value })}
+      />
+      <input
+        value={course.number}
+        className="form-control"
+        onChange={(e) => setCourse({ ...course, number: e.target.value })}
+      />
+      <input
+        value={course.startDate}
+        className="form-control"
+        type="date"
+        onChange={(e) => setCourse({ ...course, startDate: e.target.value })}
+      />
+      <input
+        value={course.endDate}
+        className="form-control"
+        type="date"
+        onChange={(e) => setCourse({ ...course, endDate: e.target.value })}
+      />
+      <hr />
+      <button onClick={addNewCourse}>Add</button>
+      <button onClick={updateCourse}>Update</button>
       <h2>Published Courses (3)</h2> <hr />
       <div className="row">
         <div className="row row-cols-1 row-cols-md-5 g-4">
@@ -27,6 +99,22 @@ function Dashboard() {
                     }}
                   >
                     {course.name}{" "}
+                    <button
+                      onClick={(event) => {
+                        event.preventDefault();
+                        setCourse(course);
+                      }}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      onClick={(event) => {
+                        event.preventDefault();
+                        deleteCourse(course._id);
+                      }}
+                    >
+                      Delete
+                    </button>
                   </Link>
                   <p className="card-text">{course.name}</p>
                   <Link
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,27 +1,66 @@
-import { Link } from "react-router-dom";
-import Nav from "../Nav";
 import KanbasNavigation from "./Navigation";
-import Dashboard from "./Dashboard";
 import { Routes, Route, Navigate } from "react-router-dom";
+import Dashboard from "./Dashboard";
 import Courses from "./Courses";
-import "./index.css";
+import * as db from "./Database";
+import { useState } from "react";
 
 function Kanbas() {
+  const [courses, setCourses] = useState<any[]>(db.courses);
+  const [course, setCourse] = useState({
+    _id: "1234",
+    name: "New Course",
+    number: "New Number",
+    startDate: "2023-09-10",
+    endDate: "2023-12-15",
+  });
+  const addNewCourse = () => {
+    setCourses([
+      ...courses,
+      { ...course, _id: new Date().getTime().toString() },
+    ]);
+  };
+  const deleteCourse = (courseId: any) => {
+    setCourses(courses.filter((course) => course._id !== courseId));
+  };
+  const updateCourse = () => {
+    setCourses(
+      courses.map((c) => {
+        if (c._id === course._id) {
+          return course;
+        } else {
+          return c;
+        }
+      })
+    );
+  };
   return (
-    <div className="d-flex nav-bar">
-      <div className="kanbas-nav">
-        <KanbasNavigation />
-      </div>
-      <div style={{ flexGrow: 1 }} className="kanbas-content">
+    <div className="d-flex">
+      <KanbasNavigation />
+      <div>
         <Routes>
           <Route path="/" element={<Navigate to="Dashboard" />} />
           <Route path="Account" element={<h1>Account</h1>} />
-          <Route path="Dashboard" element={<Dashboard />} />
-          <Route path="Courses/:courseId/*" element={<Courses />} />
+          <Route
+            path="Dashboard"
+            element={
+              <Dashboard
+                courses={courses}
+                course={course}
+                setCourse={setCourse}
+                addNewCourse={addNewCourse}
+                deleteCourse={deleteCourse}
+                updateCourse={updateCourse}
+              />
+            }
+          />
+          <Route
+            path="Courses/:courseId/*"
+            element={<Courses courses={courses} />}
+          />
         </Routes>
       </div>
     </div>
   );
 }
-
 export default Kanbas;
