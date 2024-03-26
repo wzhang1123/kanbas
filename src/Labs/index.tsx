98b81cf8d301ad22a9d19a5c18dcf8ac3cf89719
William Zhang <wzhang12321@gmail.com>
2024-03-26 18:08:32 -0400

    finished 3.0

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import Assignment3 from "./a3";
 import JavaScript from "./a3/JavaScript";
 import Assignment4 from "./a4";
+import Assignment5 from "./a5";
 import Nav from "../Nav";
 import { Routes, Route } from "react-router-dom";
 import store from "./store";
@@ -15,10 +16,12 @@ function Labs() {
         <h1>Labs</h1>
         <Nav />
         <Link to="/Labs/a3">Assignment 3</Link> |
-        <Link to="/Labs/a4">Assignment 4</Link>
+        <Link to="/Labs/a4">Assignment 4</Link> |
+        <Link to="/Labs/a5">Assignment 5</Link>
         <Routes>
           <Route path="/a3/*" element={<Assignment3 />} />
           <Route path="a4" element={<Assignment4 />} />
+          <Route path="a5" element={<Assignment5 />} />
         </Routes>
       </div>
     </Provider>
