2059fdcd8b13df64a5c4e9348c957d977664c8cb
William Zhang <wzhang12321@gmail.com>
2024-03-26 18:55:42 -0400

    finished 3.1

--- src/Labs/a5/index.tsx
+++ src/Labs/a5/index.tsx
@@ -1,8 +1,11 @@
+import EncodingParametersInURLs from "./EncodingParametersInURLS";
+
 function Assignment5() {
   return (
     <div>
       <h1>Assignment 5</h1>
       <a href="http://localhost:4000/a5/welcome">Welcome</a>
+      <EncodingParametersInURLs />
     </div>
   );
 }
