0a0f448bcde9dd4d3eafc11a7b4b18ddc69f5067
William Zhang <wzhang12321@gmail.com>
2024-03-28 01:53:09 -0400

    finished 3.2

--- src/Labs/a5/WorkingWithObjects.tsx
+++ src/Labs/a5/WorkingWithObjects.tsx
@@ -9,6 +9,14 @@ function WorkingWithObjects() {
     score: 0,
   });
   const ASSIGNMENT_URL = "http://localhost:4000/a5/assignment";
+
+  const [module, setModule] = useState({
+    id: 4550,
+    name: "Web Development",
+    description: "Learn about web development",
+    course: "CS4550",
+  });
+  const MODULE_URL = "http://localhost:4000/a5/module";
   return (
     <div>
       <h3>Working With Objects</h3>
@@ -21,10 +29,40 @@ function WorkingWithObjects() {
         }
         value={assignment.title}
       />
+      <br />
+      <a href={`${ASSIGNMENT_URL}/score/${assignment.score}`}>Update Score</a>
+      <input
+        type="number"
+        onChange={(e) =>
+          setAssignment({ ...assignment, score: e.target.valueAsNumber })
+        }
+        value={assignment.score}
+      />
+      <br />
+      <a href={`${ASSIGNMENT_URL}/completed/${assignment.completed}`}>
+        Update Completed
+      </a>
+      <input
+        type="checkbox"
+        onChange={(e) =>
+          setAssignment({ ...assignment, completed: e.target.checked })
+        }
+        value={assignment.completed.toString()}
+      />
+      <br />
+
+      <a href={`${MODULE_URL}/name/${module.name}`}>Update Name</a>
+      <input
+        type="text"
+        onChange={(e) => setModule({ ...module, name: e.target.value })}
+        value={module.name}
+      />
+
       <h4>Retrieving Objects</h4>
       <a href="http://localhost:4000/a5/assignment">Get Assignment</a>
       <h4>Retrieving Properties</h4>
       <a href="http://localhost:4000/a5/assignment/title">Get Title</a>
+      <a href="http://localhost:4000/a5/module/name">Get Module Name</a>
     </div>
   );
 }
